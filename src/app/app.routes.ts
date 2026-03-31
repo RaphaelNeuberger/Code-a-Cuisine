78158e9facc4911c95520a28ef33a265d083ebaf
Raphael Neuberger <70220250+RaphaelNeuberger@users.noreply.github.com>
2026-03-31 14:35:48 +0200

    feat: set up component structure and routing for main pages and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'ingredients', pathMatch: 'full' },
+	{ path: 'ingredients', loadComponent: () => import('./pages/ingredients/ingredients.component').then(m => m.IngredientsComponent) },
+	{ path: 'settings', loadComponent: () => import('./pages/settings/settings.component').then(m => m.SettingsComponent) },
+	{ path: 'recipe-suggestions', loadComponent: () => import('./pages/recipe-suggestions/recipe-suggestions.component').then(m => m.RecipeSuggestionsComponent) },
+	{ path: 'recipe/:id', loadComponent: () => import('./pages/recipe-detail/recipe-detail.component').then(m => m.RecipeDetailComponent) },
+	{ path: 'library', loadComponent: () => import('./pages/recipe-library/recipe-library.component').then(m => m.RecipeLibraryComponent) },
+	{ path: 'imprint', loadComponent: () => import('./pages/imprint/imprint.component').then(m => m.ImprintComponent) },
+	{ path: '**', redirectTo: 'ingredients' }
+];
